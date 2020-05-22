0821627419d2595b3fe17a882aff06c364e9630e
PandaPlaysAll <josh.piper@live.com.au>
2020-05-23 08:41:52 +1000

    first commit

---
+++ photo_uploader_handler.php
@@ -0,0 +1,4 @@
+//Connect to DB $db = mysqli_connect("IP", "username", "password", "dbname");
+//$image = $_FILES['IMAGE']['NAME']; <--- Find a way to get image and text from form
+//Post that content into sql
+//$sql = "INSERT INTO ..."
---
+++ photo_viewer_handler.php
@@ -0,0 +1,3 @@
+//connect to DB -> $db = mysqli_connect("host", "username", "password", "DBName");
+//for loop for each image to be printed inside a tabloid format
+//get text of image + image
