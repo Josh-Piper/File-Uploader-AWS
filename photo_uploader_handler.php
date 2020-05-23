8e39573511c53e2a3c97e204e69f9ca6713a109b
PandaPlaysAll <josh.piper@live.com.au>
2020-05-24 06:27:33 +1000

    Updating PHP Uploading

--- photo_uploader_handler.php
+++ photo_uploader_handler.php
@@ -2,25 +2,49 @@
 
 <?php
 
-//Connect to DB $db = mysqli_connect("IP", "username", "password", "dbname");
-//$image = $_FILES['IMAGE']['NAME']; <--- Find a way to get image and text from form
-//Post that content into sql
-//$sql = "INSERT INTO ..."
 
+//Listen for Submission.
+if (isset($_POST['submit'])) {
 
-$serverName = "localhost:8012";
-$username = "root";
-$password = "";
-$databaseName = "Images";
-$connection = mysqli_connect($serverName, $username, $password, $databaseName) or die("unable to connect");
+    //Connect to Database
+    $serverName = "localhost:8012";
+    $username = "root";
+    $password = "";
+    $databaseName = "Images";
+    //Handle SQL connection
+    $connection = mysqli_connect($serverName, $username, $password, $databaseName) or die("unable to connect");
 
-echo "Great work!!";
-//continue to next step
+    //Render all values sent by form
+    $title = $_POST['photo-title'];
+    $description = $_POST['description-title'];
+    $photo = $_POST['upload-photo'];
+    $date = $_POST['date-title'];
 
+    //Render unchecked form (validating /wOUT JS)
+    if (empty($title)) throw new Exception("Title cannot be empty");
+    if (empty($description)) throw new Exception("Description cannot be empty");
+    if (empty($phto)) throw new Exception("Photo cannot be empty");
+    if (empty($date)) throw new Exception("Date cannot be empty");
 
-//upload successful then redirect?
-//only accept images, jpg,png, jpeg , etc
-//date validation
+    //Upload to DB
+    $uploads_dir = 'images/';
+    //Add id to name? also increment ID. How to do thing
+    $id = //GET CURRENT ID. Increment by one. If EMPTY. ID = 0. 
+    $titleDuplication = rand(100,100)."-".$title;
+    move_uploaded_file($title, $uploads_dir. '/')
 
+    //Query to Insert (SQL)
+    $sql = "INSERT INTO doSomething VALUES "
+
+    if (mysqli_query($connection, $sql)) {
+        echo "Successfully Uploaded!";
+    } else {
+        echo "Error";
+    }
+    //DATABASE NAME = Swinburne, TABLE NAME = images
+    //codeflix -> fileup
+    //database stores id, Title, Photo, Description and Date
+}
+\
 
 ?>
