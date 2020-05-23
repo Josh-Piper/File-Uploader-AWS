50d412bec6acc1ed55335f8d930b0e40e779468f
PandaPlaysAll <josh.piper@live.com.au>
2020-05-24 07:08:32 +1000

    Updating PHP file - Added SQL query

---
+++ photo_searcher_handler.php
@@ -0,0 +1,5 @@
+SELECT
+FROM 
+
+
+Input into a preadjusted table that will continue adding?
--- photo_uploader_handler.php
+++ photo_uploader_handler.php
@@ -1,18 +1,38 @@
-
-
 <?php
 
-
-//Listen for Submission.
-if (isset($_POST['submit'])) {
-
     //Connect to Database
+    function ConnectToDatabase() {
     $serverName = "localhost:8012";
     $username = "root";
     $password = "";
-    $databaseName = "Images";
-    //Handle SQL connection
-    $connection = mysqli_connect($serverName, $username, $password, $databaseName) or die("unable to connect");
+    $databaseName = "swinburne";
+    return mysqli_connect($serverName, $username, $password, $databaseName);
+    }
+
+    //Render unchecked form (validating /wOUT JS)
+    function CheckLegitimateValues(array $value) {
+        foreach ($value as $item)
+        {
+            if (empty($item)) throw new Exception("$item cannot be empty");
+        }
+    }
+
+
+
+
+
+//MAIN PROGRAM HERE
+
+
+//Handle Database connection
+if (!($connection = ConnectToDatabase())) {
+    echo "Failed to connection to Database";
+}
+//Listen for Submission.
+if (isset($_POST['submit'])) {
+
+
+
 
     //Render all values sent by form
     $title = $_POST['photo-title'];
@@ -20,31 +40,35 @@ if (isset($_POST['submit'])) {
     $photo = $_POST['upload-photo'];
     $date = $_POST['date-title'];
 
-    //Render unchecked form (validating /wOUT JS)
-    if (empty($title)) throw new Exception("Title cannot be empty");
-    if (empty($description)) throw new Exception("Description cannot be empty");
-    if (empty($phto)) throw new Exception("Photo cannot be empty");
-    if (empty($date)) throw new Exception("Date cannot be empty");
+    var $checkArray = [$title, $description, $photo, $date];
+    CheckLegitimateValues($checkArray);
+
+
+    //Search for id for query
+    $query = "SELECT * ORDER by ID LIMIT 1 FROM images";
+    $result = $connection->query($query);
+    if ($result->num_rows > 0) {
+        $id = $result["id"];
+    } else {
+        $id = 0;
+    }
 
     //Upload to DB
-    $uploads_dir = 'images/';
-    //Add id to name? also increment ID. How to do thing
-    $id = //GET CURRENT ID. Increment by one. If EMPTY. ID = 0. 
-    $titleDuplication = rand(100,100)."-".$title;
-    move_uploaded_file($title, $uploads_dir. '/')
+    $target = '/pictures';
+    $target = $target . basename($_FILES['file']['name']);
+    move_uploaded_file($_FILES['file']['tmp_name'], $target);
+    //NEED TO DO
+    //move_uploaded_file($titleDuplication, $uploads_dir. '/')
 
     //Query to Insert (SQL)
-    $sql = "INSERT INTO doSomething VALUES "
+    $sql = "INSERT INTO images(id, Title, Photo, Description, DateTaken) VALUES('$title', '$description', '$photo', '$date') ";
 
     if (mysqli_query($connection, $sql)) {
         echo "Successfully Uploaded!";
     } else {
         echo "Error";
     }
-    //DATABASE NAME = Swinburne, TABLE NAME = images
-    //codeflix -> fileup
-    //database stores id, Title, Photo, Description and Date
+
 }
-\
 
 ?>
